9422538eeffe87d74c89a86ee4ca77dd6d9bdf51
marcuxo <marcourrutiam@gmail.com>
2021-07-27 23:20:16 -0400

    chamge to connect on db

--- server/DDBB/CONN.DDBB.js
+++ server/DDBB/CONN.DDBB.js
@@ -9,5 +9,5 @@ mongoose.connect(`mongodb://${process.env.DBUSER}:${process.env.DBPASS}@200.35.1
 })
   .then(db => console.log('|=>DDBB'))
   .catch(err => console.error(err));
-  DBCONN = mongoose.connection.useDb('catalogo');
+  DBCONN = mongoose.connection.useDb('indicador_inject');
   module.exports = {DBCONN};
