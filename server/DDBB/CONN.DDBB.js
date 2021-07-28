d1ed2df499164bda52bfc8ebd6f8ab852bdf7fa8
marcuxo <marcourrutiam@gmail.com>
2021-07-27 22:49:48 -0400

    commit of begin

---
+++ server/DDBB/CONN.DDBB.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+require('dotenv').config();
+mongoose.set('useFindAndModify', false);
+//mongoose.connect('mongodb://marcuxo:abc123abc@200.35.158.174:27017/admin', {//marcuxo:abc123
+mongoose.connect(`mongodb://${process.env.DBUSER}:${process.env.DBPASS}@200.35.158.174:27017/${process.env.DBNAME}`, {//marcuxo:abc123
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useNewUrlParser: true
+})
+  .then(db => console.log('|=>DDBB'))
+  .catch(err => console.error(err));
+  DBCONN = mongoose.connection.useDb('catalogo');
+  module.exports = {DBCONN};
---
+++ server/ariztindicador.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const fileupload = require('express-fileupload');
+const expHBS = require('express-handlebars');
+const app = express();
+require('./DDBB/CONN.DDBB')
+
+
+// settings
+app.set('port', 4084);
+// app.set('views', path.join(__dirname, 'views'));
+// app.engine('.hbs', expHBS({
+//   defaultLayout: 'injectmain',
+//   layoutsDir: path.join(app.get('views'), 'principal'),
+//   partialsDir: path.join(app.get('views'), 'bodynjection'),
+//   extname: '.hbs'
+// }));
+// app.set('view engine', '.hbs');
+
+app.use(cors());
+app.use(fileupload());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+
+// app.use('/paico', require('./rutas/Paico'));
+//app.use('/ochagavia', require('./rutas/Ochagavia'));
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.listen(app.get('port'), ()=> {
+  console.log(`|=>${app.get('port')}`);
+});
