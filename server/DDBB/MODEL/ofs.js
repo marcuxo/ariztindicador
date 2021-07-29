4adcc3b2139495acf47243e6f54c710decb0c9d2
Marco Urrutia <marcourrutiam@gmail.com>
2021-07-29 16:59:34 -0400

    change in some modules, DDBB query

--- server/DDBB/MODEL/ofs.js
+++ server/DDBB/MODEL/ofs.js
@@ -6,6 +6,7 @@ const ofSchema = new Schema({
   "PLANTA": { type: String, default: "129"},
   "N_OF": { type: Number },
   "FECHA_SACRIF": { type: Date, default:"NO-FECHA" },
+  "FECHA_OF": { type: Date, default:"NO-FECHA" },
   "FECHA_PRODUCT": { type: Date, default:"NO-FECHA" },
   "COD_ARTICULO": { type: String },
   "NOM_ARTICULO": { type: String },
