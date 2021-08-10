0feef033a942657a2ffd54a26d68c55050ade8e9
Marco Urrutia <marcourrutiam@gmail.com>
2021-08-10 11:56:05 -0400

    optimize and code clean

--- server/rutas/Paico.js
+++ server/rutas/Paico.js
@@ -22,7 +22,7 @@ router.get('/', async (req, res) => {
   res.render('login/formulario1',{user})
 });
 
-//1.- menu **EN DESARROLLO MODULO DE NUEVA SELECCION DE OF POR PRODUCTO///en observaci0on el metodo de menu
+//1.- menu **EN DESARROLLO MODULO DE NUEVA SELECCION DE OF POR PRODUCTO// ruta en observacion 
 router.post('/menu', async(req, res) => {
   const OPERARIO = req.body.OPERARIO;
   const codigo = req.body.producto;
