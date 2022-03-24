6dbcaa31442c2948cdb925034685d545ec0f4385
marcuxo <marcourrutiam@gmail.com>
2022-03-23 21:20:06 -0300

    delete commit

--- server/DDBB/CONN.DDBB.js
+++ server/DDBB/CONN.DDBB.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose');
 require('dotenv').config();
 mongoose.set('useFindAndModify', false);
-//mongoose.connect('mongodb://marcuxo:abc123abc@200.35.158.174:27017/admin', {//marcuxo:abc123
-mongoose.connect(`mongodb://${process.env.DBUSER}:${process.env.DBPASS}@200.35.158.174:27017/${process.env.DBNAME}`, {//marcuxo:abc123
+
+mongoose.connect(`mongodb://${process.env.DBUSER}:${process.env.DBPASS}@200.35.158.174:27017/${process.env.DBNAME}`, {
   useUnifiedTopology: true,
   useCreateIndex: true,
   useNewUrlParser: true
